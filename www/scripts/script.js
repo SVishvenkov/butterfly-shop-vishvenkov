f2b92158988120fc7b9006a818057e2f02d19400
SVishvenkov <svishvenkov@mail.ru>
2022-04-21 16:24:09 +0300

    tabs

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -4,4 +4,58 @@ $(document).ready(function () {
   $(".j-burger").on("click", function () {
     $(".j-menu").slideToggle();
   });
+
+  // табы в контактах
+
+  $(".j-tabs-link").on("click", function (event) {
+    event.preventDefault();
+
+    $(".j-tabs-link").removeClass("active");
+    $(this).addClass("active");
+
+    let index = $(this).index(".j-tabs-link");
+
+    $(".j-contacts-item").removeClass("active");
+    $(".j-contacts-item").eq(index).addClass("active");
+  });
+
+  //фильтр работ//
+
+  $(".j-catalog-search").on("click", function (event) {
+    event.preventDefault();
+
+    let filterData = $(this).data("filter");
+
+    console.log(filterData);
+
+    if (filterData === "all") {
+      $(".features-card").show();
+      return;
+    }
+
+    $(".features-card").each(function () {
+      let typeData = $(this).data("type");
+
+      if (filterData === typeData) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+  //faq
+
+
+
+  $(".j-faq-burger1").on("click", function () {
+    $(".j-desc1").slideToggle();
+  });
+  
+  $(".j-faq-burger2").on("click", function () {
+    $(".j-desc2").slideToggle();
+  });
+
+  $(".j-faq-burger3").on("click", function () {
+    $(".j-desc3").slideToggle();
+  });
 });
